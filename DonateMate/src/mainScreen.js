63d27b555dd3cf4c6ecfe5e98cc3004361ca23d3
Ahmed AK <ahmedakheirallah@gmail.com>
2023-07-16 21:04:00 +0400

    Google Forms

--- DonateMate/src/mainScreen.js
+++ DonateMate/src/mainScreen.js
@@ -2,79 +2,11 @@ import './mainScreen.css';
 import logo from "./images/logo.png"
 import usaFlag from "./images/usa.png"
 import mainImg from "./images/mainImg.png"
-import React from 'react';
+import React, {useState} from 'react';
 import YoutubeEmbed from './youtubeEmbed';
-
+import { Link } from 'react-router-dom';
 function App() {
-  const [isOpenR, setIsOpenR] = useState(false);
-  const RequestForm = ({ setIsOpenR }) => {
-   return (
-      <>
-      <div class="form">
-        <row>
-          <img class="formlogo" src="logo.png"/>
-          <p class="formlogoname">Donate Mate</p>
-        </row>
-        
-        <input class="emailtxt" type='email' placeholder='Enter email address'/>
-        <p class="bigLet" style={{marginTop: '3vh'}}>What do you need?</p>
-        <row>
-          <button class="needDevices">Phone</button>
-          <button class="needDevices">Laptop</button>
-          <button class="needDevices" style={{color: '#4d4d4d', borderStyle: 'solid', borderColor: '#a1a1a1'}}>Other</button>
-        </row>
-        <p class="bigLet" style={{marginTop: '2vh'}}>Where?</p>
-        <select class="dropdown">
-          <option>Select a pick-up location</option>
-          <option>123 Address Dr</option>
-          <option>321 Address Dr</option>
-          <option>132 Address Dr</option>
-          <option>312 Address Dr</option>
-        </select>
-        <p class="bigLet" style={{marginTop: '2vh', marginBottom: '2vh'}}>Why?</p>
-        <input class="situcond" type="text" placeholder='Describe your situation (What is the device for?)'/>
-        <row style={{marginTop: '1.5vh'}}>
-          <button class="back" onClick={() => setIsOpenR(false)}>Back</button>
-          <button class="submit" onClick={() => setIsOpenR(false)}>Submit</button>
-        </row>
-      </div>
-      </>
-    );
-  };
-  const [isOpenD, setIsOpenD] = useState(false);
-  const DonateForm = ({ setIsOpenD }) => {
-    return (
-      <>
-      <div class="form">
-        <row>
-          <img class="formlogo" src="logo.png"/>
-          <p class="formlogoname">Donate Mate</p>
-        </row>
-        <input class="emailtxt" type='email' placeholder='Enter email address'/>
-        <p class="bigLet" style={{marginTop: '3vh'}}>What do you have?</p>
-        <row>
-          <button class="needDevices">Phone</button>
-          <button class="needDevices">Laptop</button>
-          <button class="needDevices" style={{color: '#4d4d4d'}}>Other</button>
-        </row>
-        <p class="bigLet" style={{marginTop: '2vh'}}>Where?</p>
-        <select class="dropdown">
-          <option>Select a drop-off location</option>
-          <option>123 Address Dr</option>
-          <option>321 Address Dr</option>
-          <option>132 Address Dr</option>
-          <option>312 Address Dr</option>
-        </select>
-        <p class="bigLet" style={{marginTop: '2vh', marginBottom: '2vh'}}>Condition</p>
-        <input class="situcond" type="text" placeholder='Describe device condition (Is there any damage?)'/>
-        <row style={{marginTop: '1.5vh'}}>
-          <button class="back" onClick={() => setIsOpenD(false)}>Back</button>
-          <button class="submit" onClick={() => setIsOpenD(false)}>Submit</button>
-        </row>
-      </div>
-      </>
-    );
-  };
+
 
   return (
     
@@ -88,15 +20,14 @@ function App() {
       </header>
 
       <div className="buttonRow">
-        <button id="needDevice" className="buttonMain" onClick={() => setIsOpenR(true)}>
+        <Link target={"_blank"} to={"https://forms.gle/y7QJjUBmKSo21nUL8"} id="needDevice" className="buttonMain" >
           Need a device?
-        </button>
-        <button id="giveDevice" className="buttonMain" onClick={() => setIsOpenD(true)}>
+        </Link>
+        <Link target={"_blank"} to={"https://forms.gle/wrLij24iqBhx57oF9"} id="giveDevice" className="buttonMain" >
           Donate or Share
-        </button>
+        </Link>
       </div>
-        {isOpenR && <RequestForm setIsOpenR={setIsOpenR}/>}
-        {isOpenD && <DonateForm setIsOpenD={setIsOpenD}/>}
+
 
       <div className='mainImg-div'>
         <img className="mainImg" src={mainImg}/>
@@ -134,11 +65,11 @@ function App() {
       <row>
         <row style={{margin: '0 8vh 0 8vh'}}>
           <p className="needwantto">Need to borrow?</p>
-          <button className="subbuttons" onClick={() => setIsOpenR(true)}>Make a request</button> 
+          <Link target={"_blank"} to={"https://forms.gle/y7QJjUBmKSo21nUL8"} className="subbuttons" >Make a request</Link> 
         </row>
         <row style={{margin: '0 8vh 0 8vh'}}>
           <p className="needwantto">Want to donate?</p>
-          <button className="subbuttons" onClick={() => setIsOpenD(true)}>Plan a donation</button> 
+          <Link target={"_blank"} to={"https://forms.gle/wrLij24iqBhx57oF9"} className="subbuttons" >Plan a donation</Link> 
         </row>
       </row>
 
@@ -206,26 +137,26 @@ function App() {
           <div className="devices">
             <img className="devicePic" src="phone.png"/>
             <row style={{margin: '1vh 0 0 0'}}>
-              <img className="deviceIcon" src="phoneIcon.png"/>
+              <img className="deviceIcon" alt ="phone" src="phoneIcon.png"/>
               <p className="deviceName">Samsung Galaxy S5</p>
             </row>
           </div>
           <div className="devices">
-            <img className="devicePic" src="phone.png"/>
+            <img className="devicePic" alt='phone' src="phone.png"/>
             <row style={{margin: '1vh 0 0 0'}}>
-              <img className="deviceIcon" src="phoneIcon.png"/>
+              <img className="deviceIcon" alt="phone" src="phoneIcon.png"/>
               <p className="deviceName">Samsung Galaxy S5</p>
             </row>
           </div>
           <div className="devices">
-            <img className="devicePic" src="phone.png"/>
+            <img className="devicePic" alt="phone" src="phone.png"/>
             <row style={{margin: '1vh 0 0 0'}}>
-              <img className="deviceIcon" src="phoneIcon.png"/>
+              <img className="deviceIcon" alt="phone" src="phoneIcon.png"/>
               <p className="deviceName">Samsung Galaxy S5</p>
             </row>
           </div>
           <div className="devices">
-            <img className="devicePic" src="phone.png"/>
+            <img className="devicePic" alt="phone" src="phone.png"/>
             <row style={{margin: '1vh 0 0 0'}}>
               <img className="deviceIcon" src="phoneIcon.png"/>
               <p className="deviceName">Samsung Galaxy S5</p>
@@ -278,8 +209,7 @@ function App() {
         </div>
       </row>
 
-      {isOpenR && <RequestForm setIsOpenR={setIsOpenR}/>}
-      {isOpenD && <DonateForm setIsOpenD={setIsOpenD}/>}
+
     </div>
   );
 }
